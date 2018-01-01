dfb7a5701c635e5e2c264abaccc4363106b91ec3
KONDO Kazuhiro <gec01044@nifty.com>
2018-01-01 20:02:59 +0900

    rename analysis_const_draft.py to doc2vec_common.py

--- script/analysis_const_draft.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-
-class ConstDraft:
-
-    DOC2VEC_MODEL_FNAME = 'doc2vec.model'
--- script/doc2vec_query.py
+++ script/doc2vec_query.py
@@ -11,7 +11,7 @@ import argparse
 from gensim import models
 from gensim.models.doc2vec import LabeledSentence
 
-from analysis_const_draft import ConstDraft
+from doc2vec_common import Doc2VecConst
 from analysis_const import Const
 import analysis_common as common
 
@@ -39,7 +39,7 @@ if __name__ == '__main__':
     conf = parse_args()
 
     model_file = resolve_io_path(
-        conf, conf.model_file, ConstDraft.DOC2VEC_MODEL_FNAME)
+        conf, conf.model_file, Doc2VecConst.MODEL_FNAME)
     model = models.Doc2Vec.load(conf.model_file)
 
     #vector = model.docvecs[conf.query_index]
